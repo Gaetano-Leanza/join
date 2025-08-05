27a3543b54e4805f94346aa62e16f3fd23990a93
Magda Danielyan <mdanielyan50@gmail.com>
2025-08-05 17:07:36 +0200

    header and footer with nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../app/shared/header/header.component';
@@ -8,6 +9,16 @@ import { FooterComponent } from '../app/shared/footer/footer.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule, CommonModule, HeaderComponent, FooterComponent],
+=======
+import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from '../app/shared/header/header.component';
+import{FooterComponent} from '../app/shared/footer/footer.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+>>>>>>> 7002410 (header and footer with  nav)
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   template: ` <router-outlet /> `,
