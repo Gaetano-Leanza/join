d5dfc1e8c31f38bc7fbddd9e3d863e0693db9b5a
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-08-05 15:54:24 +0200

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../app/shared/header/header.component';
+import { FooterComponent } from '../app/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
   template: ` <router-outlet /> `,
 })
 export class AppComponent {
