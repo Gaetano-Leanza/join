ec382895a7a48641600a8d5247b5712138d2dfd8
Gokudoma <stephan_wacker@hotmail.com>
2025-08-06 15:03:39 +0200

    merged privacy-policy and legal-notice with navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { ContactLayoutComponent } from './features/contacts/contact-layout/contact-layout.component';
 import { ContactDetailComponent } from './features/contacts/contact-detail/contact-detail.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/contacts', pathMatch: 'full' },
   { path: 'contacts', component: ContactLayoutComponent },
   { path: 'contacts/:id', component: ContactDetailComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'legal-notice', component: LegalNoticeComponent },
 ];
