fe74abb3c6f1bf45ec9a8225b269107cda947a49
Magda Danielyan <mdanielyan50@gmail.com>
2025-08-07 17:52:34 +0200

    some changes scss

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
