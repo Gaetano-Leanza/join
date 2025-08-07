a78d0934b5bc88c8555ee9612064ba6ec9a2a900
Gokudoma <stephan_wacker@hotmail.com>
2025-08-07 15:26:37 +0200

    updated help-symbol

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) {}
+
+  isInfoPage(): boolean {
+    return this.router.url === '/info';
+  }
 
 }
