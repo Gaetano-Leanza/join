6297b1832774d1a0c370fc222b1e4469c3e05b25
Magda Danielyan <mdanielyan50@gmail.com>
2025-08-15 01:05:05 +0200

    board component with drag and drop tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.co
 import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
 import { InfoComponent } from './pages/info/info.component';
 import { AddTaskComponent } from './features/add-task/add-task.component';
+import{BoardComponent} from './features/board/board.component';
 
 /**
  * Anwendungsrouten für das Angular-Routing-Modul.
@@ -16,6 +17,7 @@ import { AddTaskComponent } from './features/add-task/add-task.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/contacts', pathMatch: 'full' },
   { path: 'task', component: AddTaskComponent },  
+  {path:"board",component:BoardComponent },
   { path: 'contacts', component: ContactLayoutComponent },
   { path: 'contacts/:id', component: ContactDetailComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
--- src/app/features/board/board.component.ts
+++ src/app/features/board/board.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+/**
+ * @title Drag&Drop connected sorting
+ */
+
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  standalone:true,
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
-export class BoardComponent {
 
+export class BoardComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+inprogress= ['In work', 'coding', 'music listening', 'eating'];
+  done = ['Get up', 'Sleeping', 'Lerning', 'Check e-mail', 'Walk dog'];
+awaitfeedback= ['Dancing', 'Shopping', 'Take a shower', 'Reading', 'Walk cat'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
