4e8ac3ee11d7b633077ec65a6022d68b63b3f8f8
Magda Danielyan <mdanielyan50@gmail.com>
2025-08-16 00:34:41 +0200

    modal window is animated

--- src/app/features/board/modal-board/modal-board.component.ts
+++ src/app/features/board/modal-board/modal-board.component.ts
@@ -1,10 +1,12 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { slideInModal } from './modal-board.animations';
 
 @Component({
   selector: 'app-modal-board',
   imports: [],
   templateUrl: './modal-board.component.html',
-  styleUrls: ['./modal-board.component.scss'] 
+  styleUrls: ['./modal-board.component.scss'] ,
+  animations: [slideInModal]
 })
 export class ModalBoardComponent {
   @Output() close = new EventEmitter<void>();
