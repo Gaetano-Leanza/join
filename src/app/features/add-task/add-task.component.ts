097d703f0c8c8e12d1a3a320c88eac72a77cffd9
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-08-16 05:13:34 +0200

    work on buttons

--- src/app/features/add-task/add-task.component.ts
+++ src/app/features/add-task/add-task.component.ts
@@ -9,15 +9,25 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./add-task.component.scss'],
 })
 export class AddTaskComponent {
-  isActive = false; // Status des Buttons
+  isActive1 = false;
+  isActive2 = false;
+  isActive3 = false;
 
-  toggleButton() {
-  this.isActive = !this.isActive; // Status umkehren
-  console.log('isActive:', this.isActive);
-}
+  toggleButton1() {
+    this.isActive1 = !this.isActive1;
+    console.log('Button 1:', this.isActive1);
+  }
 
+  toggleButton2() {
+    this.isActive2 = !this.isActive2;
+    console.log('Button 2:', this.isActive2);
+  }
 
-  // Beispiel-Daten / States
+  toggleButton3() {
+    this.isActive3 = !this.isActive3;
+    console.log('Button 3:', this.isActive3);
+  }
+
   title: string = '';
   description: string = '';
 
@@ -26,16 +36,21 @@ export class AddTaskComponent {
       console.log('Neue Aufgabe:', {
         title: this.title,
         description: this.description,
+        buttonStates: {
+          button1: this.isActive1,
+          button2: this.isActive2,
+          button3: this.isActive3,
+        },
       });
-      // Felder leeren
+
       this.title = '';
       this.description = '';
+      // Optional: Buttons zurücksetzen
+      // this.isActive1 = false;
+      // this.isActive2 = false;
+      // this.isActive3 = false;
     } else {
       alert('Bitte gib einen Titel für die Aufgabe ein.');
     }
   }
-}
-
-function PlaceholderFunktion() {
-  throw new Error('Function not implemented.');
 }
