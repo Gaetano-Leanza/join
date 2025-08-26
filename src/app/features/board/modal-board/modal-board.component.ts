80de6c6eb1a7c2026ec8d1feab37bf86d4d8d31f
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-08-26 14:51:19 +0200

    borad modal add-task

--- src/app/features/board/modal-board/modal-board.component.ts
+++ src/app/features/board/modal-board/modal-board.component.ts
@@ -1,44 +1,41 @@
-import { Component, EventEmitter, Input, Output,OnInit} from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { Task } from '../../../services/task.service';
 import { CommonModule } from '@angular/common';
-import { TaskService } from '../../../services/task.service'; 
-import{getAvatarColor,getInitials} from '../../add-task/avatar-utils';
+import { TaskService } from '../../../services/task.service';
+import { getAvatarColor, getInitials } from '../../add-task/avatar-utils';
 @Component({
   selector: 'app-modal-board',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './modal-board.component.html',
-  styleUrls: ['./modal-board.component.scss']
+  styleUrls: ['./modal-board.component.scss'],
 })
 export class ModalBoardComponent {
   getInitials = getInitials;
   getAvatarColor = getAvatarColor;
-  
 
-@Input() task!: Task;
+  @Input() task!: Task;
   @Output() close = new EventEmitter<void>();
-topContacts: any;
-Subtasks: any;
+  topContacts: any;
+  Subtasks: any;
 
-    constructor(private taskService: TaskService) {}
-    
-onDeleteTask(): void {
-  if (this.task && this.task.id) {        
-    this.taskService.deleteTask(this.task.id);
-    this.close.emit(); // Modal schließen nach dem Löschen
-  } }
+  constructor(private taskService: TaskService) {}
 
- public getCategoryColor(category: string): string {
-  switch (category) {
-    case 'User Story':
-      return ' #0038FF';     
-    case 'Technical Task':
-      return ' #1FD7C1';     
-    default:
-      return '#CCCCCC'; // Standardfarbe für unbekannte Kategorien
+  onDeleteTask(): void {
+    if (this.task && this.task.id) {
+      this.taskService.deleteTask(this.task.id);
+      this.close.emit(); // Modal schließen nach dem Löschen
+    }
   }
-}
 
-
-
+  public getCategoryColor(category: string): string {
+    switch (category) {
+      case 'User Story':
+        return ' #0038FF';
+      case 'Technical Task':
+        return ' #1FD7C1';
+      default:
+        return '#CCCCCC'; // Standardfarbe für unbekannte Kategorien
+    }
+  }
 }
