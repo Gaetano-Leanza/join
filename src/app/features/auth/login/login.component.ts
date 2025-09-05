139dd75807a918e0d8d9b0845141afb8759d8f0e
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-09-05 19:12:56 +0200

    desing login modal

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,64 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+
+  /**
+   * The name of the user.
+   * @type {string}
+   */
+  name = '';
+
+  /**
+   * The email address of the user.
+   * @type {string}
+   */
+  email = '';
+
+  /**
+   * The password for the user's account.
+   * @type {string}
+   */
+  password = '';
+
+  /**
+   * Creates an instance of the SignupComponent.
+   * @param {Router} router - The Angular Router service for navigation.
+   */
+  constructor(private router: Router) {}
+
+  /**
+   * Handles the user signup process.
+   * It logs the user's details and navigates to the login page upon completion.
+   * @returns {void}
+   */
+  login() {
+    // Your registration logic goes here
+    console.log('Name:', this.name);
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    // Navigate to the login page after successful signup
+    this.router.navigate(['/']);
+  }
+
+  /**
+   * Navigates the user back to the previous page (login page).
+   * @returns {void}
+   */
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
+
+
+
