e92ea157bbaa577991cd5586343bfb23c8c41377
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-09-07 18:09:39 +0200

    Work on Log in screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../app/shared/header/header.component';
 import { NavbarComponent } from '../app/shared/navbar/navbar.component';
 import { SplashScreenComponent } from "./splash-screen/splash-screen.component";
+import { filter } from 'rxjs/operators';
 
 /**
  * @description The root component of the application.
@@ -18,26 +19,37 @@ import { SplashScreenComponent } from "./splash-screen/splash-screen.component";
     HeaderComponent,
     NavbarComponent,
     SplashScreenComponent
-],
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 
   showSplash = true;
+  showLayout = true; // ✅ Neu: Steuert Anzeige von Navbar & Header
+
+  constructor(private router: Router) {
+    // Reagiere auf Routenwechsel
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        // ✅ Layout ausblenden auf Login & Sign-up
+        this.showLayout = !['/', '/sign-up'].includes(event.url);
+      });
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showSplash = false;
+      localStorage.setItem('splashShown', 'true');
+    }, 1200); // Dauer der Splash-Animation in ms
+  }
+
   /**
    * @description A placeholder method that is not implemented.
    * @param title The title to be processed.
    */
   title(title: any) {
     throw new Error('Method not implemented.');
-  }
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.showSplash = false;
-            localStorage.setItem('splashShown', 'true');
-
-    }, 1200); // Dauer der Splash-Animation in ms
   }
 }
